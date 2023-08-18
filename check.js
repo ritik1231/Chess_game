5a850950efa39f1623ac73dd7cb6c1e864ea04be
Sayantan Bairagi <83490141+sayantan259@users.noreply.github.com>
2023-08-18 16:14:09 +0530

    Update check.js
    Restrictions at 3 and 7 direction are also added

--- check.js
+++ check.js
@@ -250,36 +250,58 @@ function restrict(direction, row, col) {
       }
     }
   }
-
-  for (let i = 0; i < 64; i++) {
-    if (valid[i] === 1 || valid[i] === 2) {
-      if (
-        (oppoDirectionPiece === "r" || oppoDirectionPiece === "q") &&
-        (direction === 0 || direction === 4) &&
-        col != i % 8
-      ) {
-        valid[i] = 0;
-      }
-      if (
-        (oppoDirectionPiece === "r" || oppoDirectionPiece === "q") &&
-        (direction === 2 || direction === 6) &&
-        row != Math.floor(i / 8)
-      ) {
-        valid[i] = 0;
-      }
-      if (
-        (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
-        direction === 1 &&
-        (row <= Math.floor(i / 8) || col >= i % 8)
-      ) {
-        valid[i] = 0;
-      }
-      if (
-        (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
-        direction === 5 &&
-        (row >= Math.floor(i / 8) || col <= i % 8)
-      ) {
-        valid[i] = 0;
+  if (
+    (oppoDirectionPiece === "r" ||
+      oppoDirectionPiece === "q" ||
+      oppoDirectionPiece === "b") &&
+    chessPiecesString[row * 8 + col].charAt(1) === "n"
+  ) {
+    valid = [];
+  } else {
+    for (let i = 0; i < 64; i++) {
+      if (valid[i] === 1 || valid[i] === 2) {
+        if (
+          (oppoDirectionPiece === "r" || oppoDirectionPiece === "q") &&
+          (direction === 0 || direction === 4) &&
+          col != i % 8
+        ) {
+          valid[i] = 0;
+        }
+        if (
+          (oppoDirectionPiece === "r" || oppoDirectionPiece === "q") &&
+          (direction === 2 || direction === 6) &&
+          row != Math.floor(i / 8)
+        ) {
+          valid[i] = 0;
+        }
+        if (
+          (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
+          direction === 1 &&
+          (row <= Math.floor(i / 8) || col >= i % 8)
+        ) {
+          valid[i] = 0;
+        }
+        if (
+          (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
+          direction === 5 &&
+          (row >= Math.floor(i / 8) || col <= i % 8)
+        ) {
+          valid[i] = 0;
+        }
+        if (
+          (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
+          direction === 3 &&
+          (row >= Math.floor(i / 8) || col >= i % 8)
+        ) {
+          valid[i] = 0;
+        }
+        if (
+          (oppoDirectionPiece === "b" || oppoDirectionPiece === "q") &&
+          direction === 7 &&
+          (row <= Math.floor(i / 8) || col <= i % 8)
+        ) {
+          valid[i] = 0;
+        }
       }
     }
   }
