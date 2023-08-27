ada6e91004a78fc97ee1b887a3471c9ee7afaa4a
Naman Tripathi <88873021+trippin-naman@users.noreply.github.com>
2023-08-27 17:19:08 +0530

    Update checkmate.js

--- checkmate.js
+++ checkmate.js
@@ -1,42 +1,34 @@
 function checkmate(){
     let isCheckMate=true;
-    let temp=[];
 
     for(let i=0;i<63;i++){
         if(chessPiecesString[i]==''||chessPiecesString[i].charAt(0)!==chance.charAt(0)) continue;
 
         if(chessPiecesString[i].charAt(1)=='k'){
             checkKing(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else if(chessPiecesString[i].charAt(1)=='b'){
             checkBishop(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else if(chessPiecesString[i].charAt(1)=='r'){
             checkRook(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else if(chessPiecesString[i].charAt(1)=='q'){
             checkQueen(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else if(chessPiecesString[i].charAt(1)=='n'){
             checkNight(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else if(chessPiecesString[i].charAt(0)=='b'){
             checkBPawn(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
         else{
             checkWPawn(i);
-            for(let j=0;j<64;j++) if(valid[j]!=1&&valid[j]!=2) temp[j]=valid[j];
         }
     }
     
     for(let i=0;i<64;i++){
-        if(temp[i]==1||temp[i]==2){
+        if(valid[i]==1||valid[i]==2){
             isCheckMate=false;
             break;
         }
