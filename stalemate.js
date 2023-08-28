c465706221a16074efa2f7bcfa450515fbc3d890
Naman Tripathi <88873021+trippin-naman@users.noreply.github.com>
2023-08-28 21:36:18 +0530

    Add files via upload

---
+++ stalemate.js
@@ -0,0 +1,44 @@
+function stalemate(){
+    let isStaleMate=true;
+
+    if(chance=='white') checkKing(wKingPos);
+    else checkKing(bKingPos);
+
+    for(let i=0;i<64;i++){
+        if(chessPiecesString[i]==''||chessPiecesString[i].charAt(0)!==chance.charAt(0)) continue;
+
+        if(chessPiecesString[i].charAt(1)=='k') continue;
+            
+        else if(chessPiecesString[i].charAt(1)=='b'){
+            checkBishop(i);
+        }
+        else if(chessPiecesString[i].charAt(1)=='r'){
+            checkRook(i);
+        }
+        else if(chessPiecesString[i].charAt(1)=='q'){
+            checkQueen(i);
+        }
+        else if(chessPiecesString[i].charAt(1)=='n'){
+            checkNight(i);
+        }
+        else if(chessPiecesString[i].charAt(0)=='b'){
+            checkBPawn(i);
+        }
+        else{
+            checkWPawn(i);
+        }
+    }
+    
+    for(let i=0;i<64;i++){
+        if(valid[i]==1||valid[i]==2){
+            isStaleMate=false;
+            break;
+        }
+    }
+
+    if(isStaleMate==true){
+        console.log('Draw!');
+    }
+
+    valid=[];
+}
