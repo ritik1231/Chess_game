202cb409e516d17dcb819d9ea84be6cd52353bba
Naman Tripathi <88873021+trippin-naman@users.noreply.github.com>
2023-09-01 15:07:52 +0530

    Update stalemate.js

--- stalemate.js
+++ stalemate.js
@@ -1,15 +1,19 @@
 function stalemate(){
     let isStaleMate=true;
-
+    let temp=[];
+    
     if(chance=='white') checkKing(wKingPos);
     else checkKing(bKingPos);
+    temp=valid;
 
     for(let i=0;i<64;i++){
         if(chessPiecesString[i]==''||chessPiecesString[i].charAt(0)!==chance.charAt(0)) continue;
 
         if(chessPiecesString[i].charAt(1)=='k') continue;
-            
-        else if(chessPiecesString[i].charAt(1)=='b'){
+        
+        valid=[];
+
+        if(chessPiecesString[i].charAt(1)=='b'){
             checkBishop(i);
         }
         else if(chessPiecesString[i].charAt(1)=='r'){
@@ -27,10 +31,15 @@ function stalemate(){
         else{
             checkWPawn(i);
         }
+
+        for(let i=0;i<64;i++){
+            if(temp[i]==1||temp[i]==2) continue;
+            temp[i]=valid[i];
+        }
     }
     
     for(let i=0;i<64;i++){
-        if(valid[i]==1||valid[i]==2){
+        if(temp[i]==1||temp[i]==2){
             isStaleMate=false;
             break;
         }
@@ -38,7 +47,7 @@ function stalemate(){
 
     if(isStaleMate==true){
         setTimeout(function(){
-            alert('Match Drew!');
+            alert('It is a Draw!');
         },100);
     }
 
